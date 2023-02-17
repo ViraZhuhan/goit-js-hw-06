7c4d48edf1d3bc3a05fade1778d84ccdb9a539ff
ViraZhuhan <virazhuhan@gmail.com>
2023-02-17 17:43:59 +0200

    Ex 1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,1 +1,24 @@
 
+
+const allTitles = document.querySelectorAll('.title');
+console.log(`Number of categories: ${allTitles.length}`);
+console.log('');
+
+console.log(`Category: ${allTitles[0].textContent}`);
+const animalsItem = document.querySelectorAll('.animals__item');
+console.log(`Elements: ${animalsItem.length}`);
+console.log('');
+
+console.log(`Category: ${allTitles[1].textContent}`);
+const productsItem = document.querySelectorAll('.products__item');
+console.log(`Elements: ${productsItem.length}`);
+console.log('');
+
+console.log(`Category: ${allTitles[2].textContent}`);
+const technologiesItem = document.querySelectorAll('.technologies__item');
+console.log(`Elements: ${technologiesItem.length}`);
+console.log('');
+
+
+
+
