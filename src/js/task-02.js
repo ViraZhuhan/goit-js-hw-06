ff7458a268c387a8cead8028b8aa50b4d22f6620
ViraZhuhan <virazhuhan@gmail.com>
2023-02-20 13:37:56 +0200

    task-2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('ul[id=ingredients]');
+list.classList.add('ingredients');
+
+const items = ingredients.map (element => {
+const itemEl = document.createElement('li');
+itemEl.classList.add('item');
+itemEl.textContent = element;
+list.append(itemEl)
+})
+
+console.log(list);
+
+
+
+
