47e975d567db3a352fa96edd1fa3bfd124678431
ViraZhuhan <virazhuhan@gmail.com>
2023-02-21 21:27:33 +0200

    add task-3

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -1,33 +1,36 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
 
-const listEl = document.querySelector('.gallery');
+const listEl = document.querySelector(".gallery");
 
-const items = images.map(element => {
-  const itemsEl = document.createElement('li');
+const items = images.map((element) => {
+  const itemsEl = document.createElement("li");
 
-  const imageEl = document.createElement('img');
+  const imageEl = document.createElement("img");
   imageEl.src = element.url;
   imageEl.alt = element.alt;
-  imageEl.width = 200;
+  imageEl.width = 250;
 
   itemsEl.append(imageEl);
-  
+
   return itemsEl;
-  })
+});
 
-  listEl.append(...items);
+listEl.append(...items);
 
-  console.log(listEl);
+listEl.insertAdjacentHTML(
+  "beforebegin",
+  '<h2 class="title">my favorite animals</h2>'
+);
