13ac0f003854bcc1ded2fcf32f5b4a22495b1149
ViraZhuhan <virazhuhan@gmail.com>
2023-02-26 22:49:45 +0200

    add task-5

---
+++ src/js/task-05.js
@@ -0,0 +1,6 @@
+const textInput = document.querySelector('[id="name-input"]');
+const output = document.querySelector('[id="name-output"]');
+
+textInput.addEventListener("input", (event) => {
+    output.textContent = event.currentTarget.value;
+  });
