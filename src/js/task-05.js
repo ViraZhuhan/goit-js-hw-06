767e6bf1a6128f110dca74c6f9208c54326bce9b
ViraZhuhan <virazhuhan@gmail.com>
2023-02-28 17:33:55 +0200

    update task-5

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,6 +1,20 @@
 const textInput = document.querySelector('[id="name-input"]');
-const output = document.querySelector('[id="name-output"]');
+const textOutput = document.querySelector('[id="name-output"]');
 
 textInput.addEventListener("input", (event) => {
-    output.textContent = event.currentTarget.value;
+    textOutput.textContent = event.currentTarget.value;
   });
+
+
+//! OR
+
+  // const refs = {
+  // input: document.querySelector('[id="name-input"]'),
+  // output: document.querySelector('[id="name-output"]'),
+  // }
+
+  // refs.input.addEventListener('input', onInputChange);
+
+  // function onInputChange (event) {
+  //   refs.output.textContent = event.currentTarget.value;
+  // }
