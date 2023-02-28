f5d6321fe4971948b915def8670f4a3f46a0a435
ViraZhuhan <virazhuhan@gmail.com>
2023-02-28 18:32:22 +0200

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,9 +1,8 @@
 const form = document.querySelector('.login-form');
 
-form.addEventListener("submit", handleSubmit);
-
-function handleSubmit(event) {
+form.addEventListener ('submit', (event) => {
   event.preventDefault();
+  
   const {
     elements: { email, password }
   } = event.currentTarget;
@@ -14,7 +13,28 @@ function handleSubmit(event) {
 
   console.log(`Email: ${email.value}, Password: ${password.value}`);
   event.currentTarget.reset();
-}
+})
 
+//!OR
+
+// form.addEventListener ('submit', (event) => {
+//     event.preventDefault();
+
+//     const formData = new FormData(event.currentTarget);
+
+//     const emailEl = form.querySelector('input[name="email"]');
+//     const passwordEl = form.querySelector('input[name="password"]');
+
+//     formData.set(emailEl);
+//     formData.set(passwordEl);
+
+  
+//     if (emailEl === "" || passwordEl === "") {
+//       return console.log("Please fill in all the fields!");
+//     }
+  
+//     console.log(`Email: ${emailEl}, Password: ${passwordEl}`);
+//     event.currentTarget.reset();
+//   })
 
 
