8de4316dbf54c19bf0b48974ce6f553ccf5f8f05
ViraZhuhan <virazhuhan@gmail.com>
2023-03-01 15:40:02 +0200

    update task-8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,40 +1,40 @@
 const form = document.querySelector('.login-form');
 
-form.addEventListener ('submit', (event) => {
-  event.preventDefault();
-  
-  const {
-    elements: { email, password }
-  } = event.currentTarget;
-
-  if (email.value === "" || password.value === "") {
-    return console.log("Please fill in all the fields!");
-  }
-
-  console.log(`Email: ${email.value}, Password: ${password.value}`);
-  event.currentTarget.reset();
-})
-
-//!OR
-
 // form.addEventListener ('submit', (event) => {
-//     event.preventDefault();
-
-//     const formData = new FormData(event.currentTarget);
-
-//     const emailEl = form.querySelector('input[name="email"]');
-//     const passwordEl = form.querySelector('input[name="password"]');
-
-//     formData.set(emailEl);
-//     formData.set(passwordEl);
-
+//   event.preventDefault();
   
-//     if (emailEl === "" || passwordEl === "") {
-//       return console.log("Please fill in all the fields!");
-//     }
-  
-//     console.log(`Email: ${emailEl}, Password: ${passwordEl}`);
-//     event.currentTarget.reset();
-//   })
+//   const {
+//     elements: { email, password }
+//   } = event.currentTarget;
+
+//   if (email.value === "" || password.value === "") {
+//     return console.log("Please fill in all the fields!");
+//   }
+
+//   console.log(`Email: ${email.value}, Password: ${password.value}`);
+//   event.currentTarget.reset();
+// })
+
+
+//! OR:
+
+
+form.addEventListener ('submit', (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+
+    if (formData.get('email') === "" || formData.get('password') === "") {
+      return console.log("Please fill in all the fields!");}
+
+    console.log(`Email: ${formData.get('email')}, Password: ${formData.get('password')}`);  
+    
+    event.currentTarget.reset();
+  })
+
+
+
+
+
 
 
