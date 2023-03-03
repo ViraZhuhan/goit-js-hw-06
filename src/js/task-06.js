1baa62ca0ac7699c9f0b92eeb87a82729b492513
ViraZhuhan <virazhuhan@gmail.com>
2023-03-03 22:54:06 +0200

    Update task-06.js

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,15 +1,25 @@
 const validationEl = document.querySelector('[id="validation-input"]');
 
+validationEl.addEventListener("blur", onColorBlur);
 
-validationEl.addEventListener("blur", () => {
-
-  validationEl.value.length === Number(validationEl.getAttribute("data-length"))
+function onColorBlur (event) {
+ validationEl.value.length === Number(validationEl.getAttribute("data-length"))
   ? validationEl.style.borderColor = 'rgb(76, 175, 80)' 
-  : validationEl.style.borderColor = '#f44336'
-});
+  : validationEl.style.borderColor = '#f44336' 
+}
 
 
+//!OR
 
+// document.querySelector('[id="validation-input"]').onblur = function() {
+//   if (this.getAttribute('data-length') > this.value.length) { 
+//     this.classList.remove('valid');
+//     this.classList.add('invalid');
+//   } else {
+//     this.classList.remove('invalid');
+//     this.classList.add('valid');
+//   }
+// };
 
 
 
