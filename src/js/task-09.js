8a977fedf3cd9435aec8723dc583473d18aab87f
ViraZhuhan <virazhuhan@gmail.com>
2023-03-04 09:38:24 +0200

    add task-9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,19 @@
+
+const span = document.querySelector('.color');
+const btn = document.querySelector('.change-color')
+const body = document.querySelector('body')
+
+
+
+btn.addEventListener('click', onRandomColor) 
+
+function onRandomColor() {
+span.textContent = getRandomHexColor();
+body.style.backgroundColor = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
